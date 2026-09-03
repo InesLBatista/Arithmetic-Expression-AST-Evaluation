21c7453f71ac0f3ff03c7f64f95a72c4e1354842
Inês Batista <ines.s.batista@ua.pt>
2026-09-03 02:47:29 +0100

    char->token function to classify characters based on their content. mapped right into :number, :space, :unknown

--- lexer.clj
+++ lexer.clj
@@ -6,3 +6,12 @@
   (and (char? ch)
        ;;\0=48 and \9=57 so just needs to be in between
        (<= (int \0) (int ch) (int \9))))
+
+
+(defn char->token
+  "Maps a character to its token type"
+  [ch]
+  (cond
+    (digit? ch) :number
+    (= ch \space) :space
+    :else :unknow))
