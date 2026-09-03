ff8f39685ea5e55226ecefc4b259629b525f07eb
Inês Batista <ines.s.batista@ua.pt>
2026-09-03 03:03:33 +0100

    implemented a loop that traveses each character converting it to token type, to return list of token types. currently treats each digit as seperate token.

--- lexer.clj
+++ lexer.clj
@@ -15,3 +15,18 @@
     (digit? ch) :number
     (= ch \space) :space
     :else :unknow))
+
+(defn tokenize
+  "Transforms a string into a list of tokens"
+  [expression]
+   (loop [chars (seq expression)
+          tokens []]
+     ;;if nil then end of string 
+     (if (nil? chars)
+       tokens  
+       ;;still characters left
+       (let [ch (first chars)
+             remaining (rest chars)]
+         ;;restart loop only with new values
+         (recur remaining
+                (conj tokens (char->token ch)))))))
