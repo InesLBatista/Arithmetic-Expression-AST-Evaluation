aeb5c261f7081f3296151854d1fbf9d75dbad07c
Inês Batista <ines.s.batista@ua.pt>
2026-09-04 16:25:44 +0100

    replaced concat with into to maintain vector structure and rseq for efficient stack flushing. fixes the RPN output order for all expressions

--- parser.clj
+++ parser.clj
@@ -24,10 +24,18 @@
   (let [top-prec (operator-precedence top-operator)
         current-prec (operator-precedence current-operator)]
     (and (not= top-operator \()
-               (<= current-prec top-prec))))
+         (<= current-prec top-prec))))
+
+(defn pop-while
+  [pred stack]
+  (loop [s stack
+         popped []]
+    (if (and (seq s) (pred (peek s)))
+      (recur (pop s) (conj popped (peek s)))
+      [popped s])))
 
 (defn token->rpn
-  "Shunting Yard algothrim"
+  "Shunting Yard algorithm"
   [tokens]
   ;;starts with a complete list of tokens, reducing in each iteration. operator-stack keeps temporarily for output to build the result in RPN
   (loop [remaining tokens
@@ -35,7 +43,7 @@
          output []]
     (if (empty? remaining)
       ;;when the tokens end, function combines the current output with the remaining operators from the operator stack, but reverses the stack so that the top element comes out first
-      (concat output (reverse operator-stack))
+      (into output (rseq operator-stack))
 
       (let [token (first remaining)
             rest-tokens (rest remaining)]
@@ -45,42 +53,44 @@
           (recur rest-tokens
                  operator-stack
                  (conj output token))
-          
+
           ;;opening parenthesis, stacks
           (= token [:paren \(])
           (recur rest-tokens
                  (conj operator-stack token)
                  output)
-          
+
           ;;closing parenthesis, pops until finding the opening one 
           (= token [:paren \)])
           ;;it traverses the inverted stack from top to bottom, removing all operators (take-while) until it finds the parenthesis (
-          (let [popped (take-while #(not= % [:paren \(]) (reverse operator-stack))
-                remaining-stack (drop (count popped) operator-stack)
-                ;;removes the opening from the stack
-                final-stack (rest remaining-stack)]
+          (let [[popped remaining-stack] (pop-while #(not= % [:paren \(]) operator-stack)
+                final-stack (pop remaining-stack)] ;;removes the opening from the stack
             (recur rest-tokens
                    final-stack
-                   (concat output popped)))
-          
+                   (into output popped)))
+
           ;;to extract from the stack the item in it has to be an operator and needs to have higher or equal precedence to the current operator 
           (is-operator? token)
           (let [current-op (second token)
                 pop-condition (fn [stack-op]
                                 (and (is-operator? stack-op)
-                                     (should-pop-operator? (second stack-op) current-op)))]
+                                     (should-pop-operator? (second stack-op) current-op)))
+                [popped remaining-stack] (pop-while pop-condition operator-stack)]
             ;;if stack is empty, keeps the new operator in the top
             (if (empty? operator-stack)
               (recur rest-tokens
                      (conj operator-stack token)
                      output)
               ;;not being empty, removes from stack's top the pop-condition's operators. adds the operators from output, puts the current operator in the top of remaining-stack
-              (let [popped (take-while pop-condition (reverse operator-stack))
-                    remaining-stack (drop (count popped) operator-stack)]
-                (recur rest-tokens
-                       (conj remaining-stack token)
-                       (concat output popped)))))
+              (recur rest-tokens
+                     (conj remaining-stack token)
+                     (into output popped))))
           :else
           (recur rest-tokens
                  operator-stack
                  output))))))
+
+(defn parse
+  "Takes a list of tokens and returns the expression in RPN."
+  [tokens]
+  (token->rpn tokens))
